28cfcfc931dca97e4a09e3600cd992554512f48b
osman <usmanov@itu.edu.tr>
2017-07-16 00:20:35 +0300

    Update vec.cpp

--- vec.cpp
+++ vec.cpp
@@ -11,6 +11,7 @@ int main ()
     cout<<"Enter the size of quadratic 2D matrix  :  ";
     cin >> sz;
     vector<vector<float> >matric;
+    vector<vector<float> >gaelmatr; // Gauss elimination matrix
 
     for (int i=0; i<sz; i++)
     {
@@ -36,6 +37,58 @@ for (int i=0; i < matric.size(); i++)
         cout<<setw(4) << matric[i][j] ;
     }
     cout<<endl;
+
 }
 
+
+
+for (int i=0; i<sz; i++)
+    {
+        vector<float>temp;
+        for (int j=0; j<sz; j++)
+        {
+           
+            temp.push_back(matric[i][j]);
+        }
+        gaelmatr.push_back(temp);
+    }
+
+
+
+
+
+for (int i=0; i < gaelmatr.size(); i++)
+{
+
+    float temp = gaelmatr[i][0];
+    
+    for(int j=0; j < gaelmatr.size(); j++)
+    {
+
+
+        gaelmatr[i][j] = ( (gaelmatr[i][j])/ (temp));
+        
+        
+    }
+}
+
+
+
+
+
+
+
+cout<<endl;
+cout<<"////////////////////////////////////////////////////////////////////////////////"<<endl;
+cout<<endl;
+
+for (int i=0; i < gaelmatr.size(); i++)
+{
+    for(int j=0; j < gaelmatr.size(); j++)
+    {
+        cout<<setprecision(2)<<setw(6) << gaelmatr[i][j] ;
+    }
+    cout<<endl;
+
+}
 }
